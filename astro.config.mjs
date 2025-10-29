649533f50dc47a395898ab524644d7cde0e0c130
Stephen Whitfield <swhitf@deqo.io>
2025-10-29 00:52:37 +0000

    Trying CI

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://swhitf.github.io',
+  base: '/pb-site',
   vite: {
     plugins: [tailwindcss()]
   }
